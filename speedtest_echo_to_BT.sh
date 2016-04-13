0d40e6fcf4829e18bca10fdb0074cce5fca78c9e
Lee <lee@cunningnet.com>
2016-04-12 23:20:45 -0400

    change serial device name for testing
    changed serial device name for testing on shop computer

--- speedtest_echo_to_BT.sh
+++ speedtest_echo_to_BT.sh
@@ -1,1 +1,1 @@
-echo "`curl -f -w "%{speed_download}" http://speedtest.wdc01.softlayer.com/downloads/test500.zip -o /dev/null` / 131072" | bc | xargs -I {} > /dev/cu.usbserial-A700dYoR
+echo "`curl -f -w "%{speed_download}" http://speedtest.wdc01.softlayer.com/downloads/test500.zip -o /dev/null` / 131072" | bc | xargs -I {} > /dev/cu.AdafruitEZ-Link8a57-SPP
