e4c358b825908242a8c038f1aee4f7c089917ddd
Lee <lee@cunningnet.com>
2017-12-14 00:46:10 -0500

    Reconfigure shell script to connect to BlueFruit EZlink

--- speedtest_echo_to_BT.sh
+++ speedtest_echo_to_BT.sh
@@ -1,1 +1,1 @@
-echo "`curl -f -w "%{speed_download}" http://speedtest.wdc01.softlayer.com/downloads/test100.zip -o /dev/null` / 131072" | bc | xargs -I {} > /dev/tty.HC-05-DevB
+echo "`curl -f -w "%{speed_download}" http://speedtest.wdc01.softlayer.com/downloads/test100.zip -o /dev/null` / 131072" | bc | xargs -I {} > /dev/tty.AdafruitEZ-Link8a57-SPP
