b2277b37220c9c2354abf672d7e264d8a43c7c81
Lee <lee@cunningnet.com>
2017-12-14 02:39:54 -0500

    Change script to leverage speedtest-cli - more reliable

--- speedtest_echo_to_BT.sh
+++ speedtest_echo_to_BT.sh
@@ -1,1 +1,1 @@
-echo "`curl -f -w "%{speed_download}" http://speedtest.wdc01.softlayer.com/downloads/test100.zip -o /dev/null` / 131072" | bc | xargs -I {} > /dev/tty.AdafruitEZ-Link8a57-SPP
+speedtest-cli --simple | awk 'NR==2{print$2}' > /dev/tty.AdafruitEZ-Link8a57-SPP
